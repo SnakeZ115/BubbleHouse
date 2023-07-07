3a04d60b51360ce581df1196285c9e4618d8f1bb
ImAlyyy <alexfabri1321@gmail.com>
2023-07-07 08:20:16 -0700

    Conexion Mysql 1.2

--- BubbleHouse/src/Clases/ConexionSql.java
+++ BubbleHouse/src/Clases/ConexionSql.java
@@ -4,32 +4,41 @@ import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 
 public class ConexionSql {
 
     Connection conectar = null;
-    
     String URL = "jdbc:mysql://localhost:3306/bubblehouse?characterEncoding=latin1";
-    String USER = "root";
-    String PSWD = "paxxword";
+    String USER = " ";
     String CE = "CONEXION ESTABLECIDA";
     String CD = "ERROR DE CONEXION: ";
-    
+
     public Connection conexion() {
         try {
+            JPasswordField passwordField = new JPasswordField();
             Class.forName("com.mysql.jdbc.Driver");
-            conectar = DriverManager.getConnection(URL, USER, PSWD);
-            JOptionPane.showMessageDialog(null, CE);
+            USER = JOptionPane.showInputDialog("USUARIO: ");
+            int option = JOptionPane.showConfirmDialog(null, passwordField, "Contraseña", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+            if (option == JOptionPane.OK_OPTION) {
+                char[] contrasenaChars = passwordField.getPassword();
+                String PSWDD = new String(contrasenaChars);
+                conectar = DriverManager.getConnection(URL, USER, PSWDD);
+                JOptionPane.showMessageDialog(null, CE);
+                Statement estado = conectar.createStatement();
+            }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, CD + e.getMessage());
 
         }
         return conectar;
     }
-    
+
     public static void main(String[] args) throws SQLException {
-        ConexionSql bd =new ConexionSql();
+        ConexionSql bd = new ConexionSql();
         bd.conexion();
+
     }
 }
