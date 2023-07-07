56573d9f6258bb54d77614df9613f5795b220f64
ImAlyyy <alexfabri1321@gmail.com>
2023-07-06 22:15:02 -0700

    Prueba Conexion a SQL
    ya jaló la conexion

---
+++ BubbleHouse/src/Clases/ConexionSql.java
@@ -0,0 +1,29 @@
+package Clases;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class ConexionSql {
+
+    Connection conectar = null;
+
+    public Connection conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3306/bubblehouse?characterEncoding=latin1", "root", "paxxword");
+            JOptionPane.showMessageDialog(null, "CONEXION ESTABLECIDA");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ERROR DE CONEXION " + e.getMessage());
+
+        }
+        return conectar;
+    }
+    
+    public static void main(String[] args) throws SQLException {
+        ConexionSql bd =new ConexionSql();
+        bd.conexion();
+    }
+}
